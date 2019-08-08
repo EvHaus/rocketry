2b00520639456fe6c3cd08cce31035952aec7cf5
Ev Haus <ev@zenhub.com>
2019-08-08 03:23:12 +0000

    Adding more unit test coverage

--- src/index.js
+++ src/index.js
@@ -43,59 +43,63 @@ const configSchema = joi.object().keys({
 
 const explorer = cosmiconfig('rocketry');
 
+export const onConfigLoad = ({
+	config,
+	filepath,
+	isEmpty,
+}: {
+	config: ConfigType,
+	filepath: string,
+	isEmpty: boolean,
+}): any => {
+	if (isEmpty) return console.error(chalk.red(
+		`Configuration file ${filepath} is empty. Can't proceed.`
+	));
+
+	// Validate configuration
+	const result = joi.validate(config, configSchema);
+
+	if (result.error) return console.error(chalk.red(
+		`You have an error in your '${filepath}' configuration file:\n` +
+		`${result.error.message}.`
+	));
+
+	program
+		.command('run')
+		.description('Perform a production deployment')
+		.action(() => {
+			const r = new Rocketry(config, program);
+			r.run();
+		});
+
+	program
+		.command('version')
+		.description('Prints the current version of `rocketry`')
+		.action((): any => console.log(pkg.version));
+
+	// Warn about commands we don't support
+	program
+		.command('*')
+		.action((cmd: string) => {
+			console.error(chalk.red(`'${cmd}' is not a valid \`rocketry\` command.`));
+		});
+
+	// Warn about missing commands
+	if (!program.args || !program.args.length) {
+		console.error(chalk.red(`No commands specified. Try 'rocketry run' instead.`));
+	}
+
+	return program.parse(process.argv);
+};
+
+export const onConfigLoadError = (error: Error) => {
+	console.error(chalk.red(
+		`Unexpected error occured: ${error.message}`
+	));
+};
+
 // Search for a configuration by walking up directories.
 // See documentation for search, below.
 explorer.search(program.dir)
-	.then(({
-		config,
-		filepath,
-		isEmpty,
-	}: {
-		config: ConfigType,
-		filepath: string,
-		isEmpty: boolean,
-	}): any => {
-		if (isEmpty) return console.error(chalk.red(
-			`Configuration file ${filepath} is empty. Can't proceed.`
-		));
-
-		// Validate configuration
-		const result = joi.validate(config, configSchema);
-
-		if (result.error) return console.error(chalk.red(
-			`You have an error in your '${filepath}' configuration file:\n` +
-			`${result.error.message}.`
-		));
-
-		program
-			.command('run')
-			.description('Perform a production deployment')
-			.action(() => {
-				const r = new Rocketry(config, program);
-				r.run();
-			});
-
-		program
-			.command('version')
-			.description('Prints the current version of `rocketry`')
-			.action((): any => console.log(pkg.version));
-
-		// Warn about commands we don't support
-		program
-			.command('*')
-			.action((cmd: string) => {
-				console.error(chalk.red(`'${cmd}' is not a valid \`rocketry\` command.`));
-			});
-
-		// Warn about missing commands
-		if (!program.args || !program.args.length) {
-			console.error(chalk.red(`No commands specified. Try 'rocketry run' instead.`));
-		}
-
-		return program.parse(process.argv);
-	})
-	.catch((error: Error) => {
-		console.error(chalk.red(
-			`Unexpected error occured: ${error.message}`
-		));
-	});
+	.then(onConfigLoad)
+	.catch(onConfigLoadError);
--- src/index.test.js
+++ src/index.test.js
@@ -1,12 +1,18 @@
-import './index';
+/* eslint-disable global-require */
+
+import chalk from 'chalk';
 import program from 'commander';
 
 describe('index', () => {
 	it('should setup commander with the right version', () => {
+		spyOn(console, 'error');
+		require('./index');
 		expect(program.version).toHaveBeenCalledWith(jasmine.any(String));
 	});
 
 	it('should register the "--dir" option', () => {
+		spyOn(console, 'error');
+		require('./index');
 		expect(program.option).toHaveBeenCalledWith(
 			'-D, --dir <path>',
 			jasmine.any(String),
@@ -15,9 +21,52 @@ describe('index', () => {
 	});
 
 	it('should register the "--debug" option', () => {
+		spyOn(console, 'error');
+		require('./index');
 		expect(program.option).toHaveBeenCalledWith(
 			'-d, --debug',
 			jasmine.any(String)
+		);
+	});
+
+	it('should handle unexpected config load errors gracefully', () => {
+		const consoleSpy = spyOn(console, 'error');
+		const {onConfigLoadError} = require('./index');
+		onConfigLoadError(new Error('failure'));
+		expect(consoleSpy).toHaveBeenCalledWith(
+			chalk.red('Unexpected error occured: failure')
+		);
+	});
+
+	it('should handle empty configs gracefully', () => {
+		const consoleSpy = spyOn(console, 'error');
+		const {onConfigLoad} = require('./index');
+		onConfigLoad({config: null, filepath: 'filepath', isEmpty: true});
+		expect(consoleSpy).toHaveBeenCalledWith(
+			chalk.red(`Configuration file filepath is empty. Can't proceed.`)
+		);
+	});
+
+	it('should handle config validation errors gracefully', () => {
+		const consoleSpy = spyOn(console, 'error');
+		const {onConfigLoad} = require('./index');
+		onConfigLoad({config: {}, filepath: 'filepath', isEmpty: false});
+		expect(consoleSpy).toHaveBeenCalledWith(
+			expect.stringContaining(`You have an error in your 'filepath' configuration file`)
+		);
+	});
+
+	it('should expect a valid command if given a valid configuration file', () => {
+		const consoleSpy = spyOn(console, 'error');
+		const {onConfigLoad} = require('./index');
+		const config = {
+			host: '24.1.2.3',
+			target_dir: '/var/www/',
+			user: 'user',
+		};
+		onConfigLoad({config, filepath: 'filepath', isEmpty: false});
+		expect(consoleSpy).toHaveBeenCalledWith(
+			expect.stringContaining(`No commands specified.`)
 		);
 	});
 });
